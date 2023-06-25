0a0689de274e507ba09d11f6ebb5ca43e25e804a
Aryaman Shetty <aryamannshetty@gmail.com>
2023-06-25 11:13:40 +0530

    Fixed the hero widget , Error in the routing

--- lib/main.dart
+++ lib/main.dart
@@ -9,5 +9,6 @@ void main() => runApp(MaterialApp(
       routes: {
         '/': (context) => const QuoteList(),
         '/description': (context) => const Description(),
+        
       },
     ));
